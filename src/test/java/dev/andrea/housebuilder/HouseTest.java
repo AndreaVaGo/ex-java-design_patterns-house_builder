b6a10d18b59bf4ce34f9f5273cacccb34a413268
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-08-31 15:14:17 +0200

    Add unit tests for House

---
+++ src/test/java/dev/andrea/housebuilder/HouseTest.java
@@ -0,0 +1,38 @@
+package dev.andrea.housebuilder;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+class HouseTest {
+
+    @Test
+    void shouldHaveGarage() {
+        House house = new House();
+        house.setGarage(true);
+        assertThat(house.isGarage(), is(true));
+    }
+
+    @Test
+    void shouldHaveswimmingPool() {
+        House house = new House();
+        house.setSwimmingPool(true);
+        assertThat(house.isSwimmingPool(), is(true));
+    }
+
+    @Test
+    void shouldHavefancyStatues() {
+        House house = new House();
+        house.setFancyStatues(true);
+        assertThat(house.isFancyStatues(), is(true));
+    }
+
+    @Test
+    void shouldHavegarden() {
+        House house = new House();
+        house.setGarden(true);
+        assertThat(house.isGarden(), is(true));
+    }
+
+}
